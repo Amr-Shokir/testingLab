577f7464781e0a9525d4973c4ebe8730a1aa77cf
amrShokir <amrisa1310@gmail.com>
2025-12-07 14:32:56 +0200

    fixed bugs

--- repositories/repository_factory.py
+++ repositories/repository_factory.py
@@ -1,5 +1,5 @@
 from repositories.user_repository import UserRepository
-# from repositories.product_repository import ProductRepository
+from repositories.product_repository import ProductRepository
 
 
 class RepositoryFactory:
@@ -7,7 +7,7 @@ class RepositoryFactory:
     def get_repository(entity_type):
         if entity_type == "user":
             return UserRepository()
-        # elif entity_type == "product":
-        #     return ProductRepository()
+        elif entity_type == "product":
+            return ProductRepository()
         else:
             raise ValueError("Unknown repository type")
